c979dee3094ee2f23508c386a4bd9c0436b67439
capnemo20 <z.sujon20@outlook.com>
2024-10-01 02:19:05 +0100

    28-6 Implement cashout using function

---
+++ js/addMoney2.js
@@ -0,0 +1,9 @@
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const addMoney = getInputFieldValueById("input-add-money");
+    const pinNumber = getInputFieldValueById("input-pin-number");
+    console.log("add money inside addMoney2.js", addMoney, pinNumber);
+  });
