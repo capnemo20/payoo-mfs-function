d77c2efd89b4998a9c612e9ce03c237bd16adac9
capnemo20 <z.sujon20@outlook.com>
2024-10-03 13:46:15 +0100

    28-8 Show transactions

--- js/addMoney2.js
+++ js/addMoney2.js
@@ -10,7 +10,16 @@ document
     if (pinNumber === 1234) {
       const balance = getTextFieldValueById("account-balance");
       const newBalance = balance + addMoney;
+      //show in the UI
       document.getElementById("account-balance").innerText = newBalance;
+      //add to transaction history
+      const p = document.createElement("p");
+      p.innerText = `added ${addMoney}tk. New Balance: ${newBalance}`;
+      // console.log(p);
+      //should be a common function
+      document.getElementById("transaction-container").appendChild(p);
+
+
     } else {
       alert("failed to add money");
     }
